6e15b9b84201ea39aee29486b5e321bd327228ad
ChristianVlad <christian.valladaresp@gmail.com>
2024-09-05 11:14:20 -0600

    fixing the artifacts

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -4,7 +4,7 @@ import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
 from sklearn.model_selection import train_test_split
-from dataclasess import dataclass
+from dataclasses import dataclass
 
 @dataclass
 class DataIngestionConfig:
@@ -12,28 +12,30 @@ class DataIngestionConfig:
     test_data_path: str=os.path.join('artifacts', 'test.csv')
     raw_data_path: str=os.path.join('artifacts', 'raw.csv')
 
+path_file = r"C:\Users\christian.pena\Escritorio\mlproject\notebook\data\stud.csv"
 class DataIngestion:
     def __init__(self):
         self.ingestion_config=DataIngestionConfig()
 
     def initiate_data_ingestion(self):
-        loggin.info("Entered the data ingestion method or component")
+        logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('notebook\data\stud.csv')
+            df = pd.read_csv(path_file,sep=',')
+
             logging.info('Read the dataset as dataframe')
 
             os.makedirs(os.path.dirname(self.ingestion_config.train_data_path), exist_ok=True)
 
             df.to_csv(self.ingestion_config.raw_data_path, index=False, header=True)
 
-            loggin.info("Train test split initiated")
+            logging.info("Train test split initiated")
             train_set, test_set=train_test_split(df, test_size=0.2, random_state=42)
 
             train_set.to_csv(self.ingestion_config.train_data_path, index=False, header=True)
             
             test_set.to_csv(self.ingestion_config.test_data_path, index=False, header=True)
 
-            loggin.info("Ingestion of the data is completed")
+            logging.info("Ingestion of the data is completed")
 
             return(
                 self.ingestion_config.train_data_path,
