bc9506e70fea5f61d7e2b90fe1a9a93bb24e710f
ChristianVlad <christian.valladaresp@gmail.com>
2024-09-09 16:22:41 -0600

    Model Trainer

--- src/utils.py
+++ src/utils.py
@@ -4,7 +4,7 @@ import numpy as np
 import pandas as pd
 import dill
 from src.exception import CustomException
-from sklear.metrics import r2_score
+from sklearn.metrics import r2_score
 
 def save_object(file_path, obj):
     try:
@@ -18,7 +18,7 @@ def save_object(file_path, obj):
     except Exception as e:
         raise CustomException(e, sys)
 
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_model(X_train, y_train, X_test, y_test, models):
     try:
         report = {}
 
