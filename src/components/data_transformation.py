592f29cd62fb76be0ec2bc9af2c08cc3a3253f6e
Chrstianp <christan.valladaresp@gmail.com>
2024-09-08 22:41:15 -0600

    data transformation and pipeline

---
+++ src/components/data_transformation.py
@@ -0,0 +1,53 @@
+import os
+import sys
+import pandas as pd
+import numpy as np
+from dataclasses import dataclass
+from sklearn.compose import ColumnTransformer
+from sklearn.impute import SimpleImputer
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import OneHotEncoder, StandardScaler
+from src.exception import CustomException
+from src.logger import logging
+
+class DataTransformationConfig:
+    preprocessor_obj_file_path=os.path.join('artifacts', "preprocessor.pkl")
+
+class DataTransformation:
+    def __init__(self):
+        self.data_transformation_config=DataTransformationConfig()
+
+    def get_data_transfromer_object(self):
+        try:
+            numerical_columns =["writing_score", "reading_score"]
+            categorical_columns=["gender", "race_ethnicity", "parental_level_of_education", "luch", "test_preparation_course"]
+        
+            num_pipeline = Pipeline(
+                steps=[
+                    ("Imputer", SimpleImputer(strategy="median")),
+                    ("scaler", StandardScaler())
+            ])
+
+            cat_pipeline=Pipeline(steps=[
+                ("imputer", SimpleImputer(strategy="most_frecuent")),
+                ("one_hot_encoder", OneHotEncoder()),
+                ("scaler", StandardScaler())
+            ])
+
+            logging.info("Numerical columns standar scalling completed: {numerical_columns}")
+            logging.info("Categorical columns encodign completed: {categorical_columns}")
+
+            preprocessor=ColumnTransformer(
+                [
+                    ("num_pipeline", num_pipeline, numerical_columns),
+                    ("cat_pipeline", cat_pipeline, categorical_columns)
+                ])
+            
+            return preprocessor
+
+        except Exception as e:
+            raise CustomException(e, sys)
+
+
+
+
